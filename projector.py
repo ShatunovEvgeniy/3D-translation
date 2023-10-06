108c814c43ec5a99c48b7a81a5677384d3e0b6fa
ShatunovEvgeniy <shatunov868@gmail.com>
2023-10-06 18:07:28 +0300

    projector : class Projector with constructor added

--- projector.py
+++ projector.py
@@ -1,3 +1,14 @@
 from math import log2, trunc
 import numpy as np
 
+
+class Projector:
+    def __init__(self, position, height, width, stripe_count):
+        self.stripe_count = stripe_count
+        self.position = position  # position of the projector relative to the camera
+        self.stripes_order = self.StripeArray()  # order of stripes in each iteration
+        self.height = height  # height of projection image
+        self.width = width  # width of projection image
+        self.strip_width = width // stripe_count
+        self.image_index = 0  # index of last generated image
+

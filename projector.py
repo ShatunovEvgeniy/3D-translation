e967e167e3159a10b9a33903e5b2a64bbb6ba20c
ShatunovEvgeniy <shatunov868@gmail.com>
2023-10-06 22:21:02 +0300

    projector : FindImageCount() changed
    Small fixes according to new class' fields. The exception was deleted.

--- projector.py
+++ projector.py
@@ -59,17 +59,12 @@ class Projector:
                 A line number is number of an image and a column number is number of each stipe.
                 So (i, j) element shows if in an i-iteration a j-stripe is shown or not
         """
-        image_count = log2(self.stripe_count)  # count of projection needing for 2D->3D translation
-        if trunc(image_count) != image_count:
-            raise ValueError("Count of stripes in StripeArray(count) has to be a power of 2.")
-        image_count = int(image_count)
 
-        image_count_str = '0' + str(image_count + 2) + 'b'
+        image_count_str = '0' + str(self.image_count + 2) + 'b'
         bins = [format(i, image_count_str)[2:] for i in range(self.stripe_count)]  # array of bin numbers,
         # preparation for result array
 
-        stripe_array = np.zeros((image_count, self.stripe_count), dtype=int)
-        # there is stripe_count - 1 because a column of zeros doesn't have sense (means a stripe we never see)
+        stripe_array = np.zeros((self.image_count, self.stripe_count), dtype=int)
 
         for i in range(self.stripe_count):
             stripe_array[..., i] = np.array([int(j) for j in bins[i]]).T
